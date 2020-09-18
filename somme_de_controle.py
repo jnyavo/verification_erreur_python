fdd51bd63fdc32129d06fb1e93cbdcf216f8eb83
nyavo <johanesanyavo@gmail.com>
2020-09-18 18:31:50 +0000

    Suppression d'une fonction inutile

--- somme_de_controle.py
+++ somme_de_controle.py
@@ -2,32 +2,24 @@
 
 
 
-
-def limite_checksum(valeur):
-        #verifie si la valeur est plus de 65535
-        if (valeur > 65535):
-                return valeur % 65535
-        return valeur
-
-
 def somme_fletcher(texte):
 	checksum1 = []
 	checksum2 = []
 	somme = 0
 
-	#faire le tableau checksum1 
+	#faire le tableau checksum1
 	for i in texte:
 		#ord() retourne la valeur acsii
 		valeur = ord(i)
 		somme += valeur
-		checksum1.append(limite_checksum(somme))
+		checksum1.append(somme % 65535)
 
 	#faire le tableau checksum2
 	counter = 0
 	somme = 0
 	for i in texte:
 		somme += checksum1[counter]
-		checksum2.append(limite_checksum(somme))
+		checksum2.append(somme % 65535)
 		counter += 1
 	#retourner un dictionnaire des deux checksums
 	return {'checksum1':checksum1,'checksum2':checksum2}
